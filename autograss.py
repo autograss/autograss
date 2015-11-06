ac0d79026c2a81fce1ea77e6484688fe1ed1c8f4
David Carlos <ddavidcarlos1392@gmail.com>
2015-11-06 19:05:46 -0200

    Creates main class AutoGrass

---
+++ autograss.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+
+from os import path
+import commands
+import time
+from photo import Photo
+
+class AutoGrass:
+
+    def direction(self):
+        photo = Photo(1024,768,'0')
+        photo.take_photo()
+        movementalgorithm = "../movementalgorithm/bin/movimentalgorithm"
+        start_time1 = time.time()
+        direction = commands.getoutput(movementalgorithm + ' ' +  photo.path())
+        print("%s seconds processamento" % (time.time() - start_time1))
+        print direction
+
+
+def main():
+    start_time = time.time()
+    autograss = AutoGrass()
+    autograss.direction()
+    #print("%s seconds" % (time.time() - start_time))
+
+if __name__ == '__main__':
+    main()
