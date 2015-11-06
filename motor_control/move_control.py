e8831817477e2bfab2320102d37c9b319818a7eb
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-11-06 16:39:39 -0200

    Create class MoveControl

--- motor_control/move_control.py
+++ motor_control/move_control.py
@@ -4,87 +4,81 @@ import base_control
 import logging
 
 
-STOP_MOVE = "stop_move"
-MOVE_FOWARD = "move_foward"
-MOVE_BACKWARD = "move_backward"
-MOVE_LEFT = "move_left"
-MOVE_RIGHT = "move_right"
-ROTATE_LEFT = "rotate_left"
-ROTATE_RIGHT = "rotate_right"
+class MoveControl:
+    STOP_MOVE = "stop_move"
+    MOVE_FOWARD = "move_foward"
+    MOVE_BACKWARD = "move_backward"
+    MOVE_LEFT = "move_left"
+    MOVE_RIGHT = "move_right"
+    ROTATE_LEFT = "rotate_left"
+    ROTATE_RIGHT = "rotate_right"
 
+    def __init__(self):
+        pass
 
-def move(direction):
-    if direction == STOP_MOVE:
-        stop_move()
-    elif direction == MOVE_LEFT:
-        move_left()
-    elif direction == MOVE_RIGHT:
-        move_right()
-    elif direction == MOVE_LEFT:
-        move_left()
-    elif direction == ROTATE_LEFT:
-        rotate_left()
-    elif direction == ROTATE_RIGHT:
-        rotate_right()
+    def move(self, direction):
+        if direction == self.STOP_MOVE:
+            self.stop_move()
+        elif direction == self.MOVE_LEFT:
+            self.move_left()
+        elif direction == self.MOVE_RIGHT:
+            self.move_right()
+        elif direction == self.MOVE_LEFT:
+            self.move_left()
+        elif direction == self.ROTATE_LEFT:
+            self.rotate_left()
+        elif direction == self.ROTATE_RIGHT:
+            self.rotate_right()
 
+    def exit(self):
+        self.stop_move()
+        base_control.exit()
 
-def exit():
-    stop_move()
-    base_control.exit()
+    def stop_move(self):
+        speed_left = 0.0
+        speed_right = 0.0
 
+        self.update_speed(speed_left, speed_right)
 
-def stop_move():
-    speed_left = 0.0
-    speed_right = 0.0
+    def move_foward(self):
+        speed_left = -0.8
+        speed_right = -0.8
 
-    update_speed(speed_left, speed_right)
+        self.update_speed(speed_left, speed_right)
 
+    def move_backward(self):
+        speed_left = 0.8
+        speed_right = 0.8
 
-def move_foward():
-    speed_left = -0.8
-    speed_right = -0.8
+        self.update_speed(speed_left, speed_right)
 
-    update_speed(speed_left, speed_right)
+    def move_left(self):
+        speed_left = -0.4
+        speed_right = -0.7
 
+        self.update_speed(speed_left, speed_right)
 
-def move_backward():
-    speed_left = 0.8
-    speed_right = 0.8
+    def move_right(self):
+        speed_right = -0.4
+        speed_left = -0.7
 
-    update_speed(speed_left, speed_right)
+        self.update_speed(speed_left, speed_right)
 
+    def rotate_left(self):
+        speed_left = 0
+        speed_right = -0.8
 
-def move_left():
-    speed_left = -0.4
-    speed_right = -0.7
+        self.update_speed(speed_left, speed_right)
 
-    update_speed(speed_left, speed_right)
+    def rotate_right(self):
+        speed_left = -0.8
+        speed_right = -0
 
+        self.update_speed(speed_left, speed_right)
 
-def move_right():
-    speed_right = -0.4
-    speed_left = -0.7
+    def update_speed(self, speed_left, speed_right):
+        logging.info("speed_left: speed_left")
+        logging.info("speed_right: speed_right")
 
-    update_speed(speed_left, speed_right)
-
-
-def rotate_left():
-    speed_left = 0
-    speed_right = -0.8
-
-    update_speed(speed_left, speed_right)
-
-
-def rotate_right():
-    speed_left = -0.8
-    speed_right = -0
-
-    update_speed(speed_left, speed_right)
-
-
-def update_speed(speed_left, speed_right):
-    logging.info("speed_left: speed_left")
-    logging.info("speed_right: speed_right")
-
-    base_control.setMotorLeft(speed_left)
-    base_control.setMotorRight(speed_right)
+        base_control.setMotorLeft(speed_left)
+        base_control.setMotorRight(speed_right)
