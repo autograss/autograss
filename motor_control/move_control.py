00c84cb5291ecdd2c53ea7ec1f40858b7a55217c
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-11-10 18:44:50 -0200

    Increment value speed
    Signed-off-by: Ciro Martins <ciroms.0@gmail.com>
    Signed-off-by: Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>

--- motor_control/move_control.py
+++ motor_control/move_control.py
@@ -17,69 +17,91 @@ class MoveControl:
         self.speed_right = 0
 
     def move(self, direction):
-       	if direction == self.STOP_MOVE:
-           self.stop_move()
-      	elif direction == self.MOVE_LEFT:
-           self.move_left()
-       	elif direction == self.MOVE_RIGHT:
-           self.move_right()
-       	elif direction == self.MOVE_FORWARD:
-           self.move_forward()
-       	elif direction == self.ROTATE_LEFT:
-           self.rotate_left()
-       	elif direction == self.ROTATE_RIGHT:
-           self.rotate_right()
-	elif direction == self.MOVE_BACKWARD:
-	   self.move_backward()
+        if direction == self.STOP_MOVE:
+            self.stop_move()
+        elif direction == self.MOVE_LEFT:
+            self.move_left()
+        elif direction == self.MOVE_RIGHT:
+            self.move_right()
+        elif direction == self.MOVE_FORWARD:
+            self.move_forward()
+        elif direction == self.ROTATE_LEFT:
+            self.rotate_left()
+        elif direction == self.ROTATE_RIGHT:
+            self.rotate_right()
+        elif direction == self.MOVE_BACKWARD:
+            self.move_backward()
 
     def exit(self):
         self.stop_move()
         base_control.exit()
 
     def stop_move(self):
-        self.speed_left = 0.0
-        self.speed_right = 0.0
+        self.set_speed(0.0, 0.0)
 
-        self.update_speed()
+    def move_foward(self):
+        limit_left = 0.8
+        limit_right = 0.8
 
-    def move_forward(self):
-        self.speed_left = 0.8
-        self.speed_right = 0.8
+        increment_left = 0.05
+        increment_right = 0.05
 
-        self.update_speed()
+        self.update_speed(increment_left, increment_right, limit_left,
+                          limit_right)
 
     def move_backward(self):
-        self.speed_left =  - 0.8
-        self.speed_right = - 0.8
+        limit_left = -0.8
+        limit_right = -0.8
 
-        self.update_speed()
+        increment_left = -0.05
+        increment_right = -0.05
+
+        self.update_speed(increment_left, increment_right, limit_left,
+                          limit_right)
 
     def move_left(self):
-        self.speed_left = -0.0
-        self.speed_right = 0.8
+        limit_left = -0.4
+        limit_right = 0.8
 
-        self.update_speed()
+        increment_left = -0.05
+        increment_right = 0.05
+
+        self.update_speed(increment_left, increment_right, limit_left,
+                          limit_right)
 
     def move_right(self):
-        self.speed_right = - 0.0
-        self.speed_left = 0.8
+        limit_left = 0.8
+        limit_right = -0.4
 
-        self.update_speed()
+        increment_left = 0.05
+        increment_right = -0.05
+
+        self.update_speed(increment_left, increment_right, limit_left,
+                          limit_right)
 
     def rotate_left(self):
-        self.speed_left = 0
-        self.speed_right = 0.8
-
-        self.update_speed()
+        pass
 
     def rotate_right(self):
-        self.speed_left = 0.8
-        self.speed_right = 0
+        pass
 
-        self.update_speed()
+    def set_speed(self, speed_left, speed_right):
+        self.speed_left = speed_left
+        self.speed_right = speed_right
 
-    def update_speed(self):
-        print self.speed_left
-        print self.speed_right
+        base_control.setMotorLeft(self.speed_left)
+        base_control.setMotorRight(self.speed_right)
+
+    def update_speed(self, increment_left, increment_right, limit_left,
+                     limit_right):
+        self.speed_left = self.speed_left + increment_left
+        self.speed_right = self.speed_right + increment_right
+
+        if abs(self.speed_left) > abs(limit_left):
+            self.speed_left = limit_left
+
+        if abs(self.speed_right) > abs(limit_right):
+            self.speed_right = limit_right
+
         base_control.setMotorLeft(self.speed_left)
         base_control.setMotorRight(self.speed_right)
